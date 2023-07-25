8d87f8f36d639c5783b9eac526025070b2637721
Jagdish Kumawat <jagdishkumawat81@gmail.com>
2023-07-25 15:39:58 +0530

    fetching data UseEffect and useReducer

--- quiz-app-08/src/App.js
+++ quiz-app-08/src/App.js
@@ -1,6 +1,21 @@
 import Header from "./Header";
 import Main from "./Main";
+import { useEffect, useReducer } from "react";
+
+const initialState = {};
+
+function reducer(state, action) {}
+
 const App = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+      .catch((err) => console.log("Error : ", err));
+  }, []);
+
   return (
     <div>
       <Header />
--- quiz-app-08/src/Main.js
+++ quiz-app-08/src/Main.js
@@ -1,11 +1,7 @@
 const Main = ({ children }) => {
   console.log(children);
 
-  return (
-    <div>
-      <h1>{children}</h1>
-    </div>
-  );
+  return <div>{/* <h1>{children}</h1> */}</div>;
 };
 
 export default Main;
