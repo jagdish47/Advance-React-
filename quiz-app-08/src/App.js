0ebd4b683419bbebf0333e2d7b75bfabfd09024d
Jagdish Kumawat <jagdishkumawat81@gmail.com>
2023-07-25 16:45:49 +0530

    updation

--- quiz-app-08/src/App.js
+++ quiz-app-08/src/App.js
@@ -2,9 +2,23 @@ import Header from "./Header";
 import Main from "./Main";
 import { useEffect, useReducer } from "react";
 
-const initialState = {};
+const initialState = {
+  questions: [],
+  status: "loading",
+  //loading, error, ready, active, finished
+};
 
-function reducer(state, action) {}
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return { ...state, questions: action.payload, status: "Ready" };
+
+    case "dataFailed":
+      return { ...state, status: "Failed" };
+    default:
+      throw new Error("Action Unknown");
+  }
+}
 
 const App = () => {
   const [state, dispatch] = useReducer(reducer, initialState);
@@ -12,8 +26,8 @@ const App = () => {
   useEffect(() => {
     fetch("http://localhost:8000/questions")
       .then((res) => res.json())
-      .then((data) => console.log(data))
-      .catch((err) => console.log("Error : ", err));
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
   }, []);
 
   return (
